76b5322bca2ba375f31c3a5988b70a3879d2dac9
Bill Figliolini <BillFigliolini@gmail.com>
2025-08-18 18:45:33 -0400

    Created reserved word lookup builder

--- src/lox/common.rs
+++ src/lox/common.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 #[derive(Debug, PartialEq, Clone)]
 pub enum TokenType {
     //Single Character
@@ -62,4 +64,29 @@ impl Token {
 
 pub fn report_error(line_number: usize, error_message: &str) {
     eprintln!("[line {line_number}] Error: {error_message}");
+}
+
+fn new_reserved_words() -> HashMap<Vec<u8>, TokenType> {
+    let mut reserved_words = HashMap::new();
+    //Logical operators and booleans
+    reserved_words.insert(b"true".to_vec(), TokenType::True);
+    reserved_words.insert(b"false".to_vec(), TokenType::False);
+    reserved_words.insert(b"and".to_vec(), TokenType::And);
+    reserved_words.insert(b"or".to_vec(), TokenType::Or);
+    //Control Flow
+    reserved_words.insert(b"if".to_vec(), TokenType::If);
+    reserved_words.insert(b"else".to_vec(), TokenType::Else);
+    reserved_words.insert(b"for".to_vec(), TokenType::For);
+    reserved_words.insert(b"while".to_vec(), TokenType::While);
+    //Variable Related
+    reserved_words.insert(b"var".to_vec(), TokenType::Var);
+    reserved_words.insert(b"this".to_vec(), TokenType::This);
+    reserved_words.insert(b"super".to_vec(), TokenType::Super);
+    reserved_words.insert(b"nil".to_vec(), TokenType::Nil);
+    reserved_words.insert(b"class".to_vec(), TokenType::Class);
+    //Functions Related
+    reserved_words.insert(b"fun".to_vec(), TokenType::Fun);
+    reserved_words.insert(b"return".to_vec(), TokenType::Return);
+    reserved_words.insert(b"print".to_vec(), TokenType::Print);
+    reserved_words
 }
